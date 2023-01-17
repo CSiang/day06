824f08c0493ba89cb98f9c74e6f4273d61f0aca8
CSiang <cscorpio87@gmail.com>
2023-01-17 11:02:52 +0800

    basic threading examples

--- tfip/src/main/java/day06/App.java
+++ tfip/src/main/java/day06/App.java
@@ -7,11 +7,29 @@ public final class App {
     private App() {
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Thread thread1 = new Thread( new Runnable() {
+            @Override
+            public void run() {
+                // Here to specify the code you want to run.
+                for (int i=0; i<20; i++){
+                    System.out.println(Thread.currentThread().getName() + "\tRunnable ..." +i);
+                    // Thread.currentThread().getName() is to get the name of the thread that is running.
+                    }// end of for
+                 } // end of run
+            } ); // end of runnable
+
+        thread1.start();
+
+        RunnableImplement mRI = new RunnableImplement ();
+        Thread thread2 = new Thread(mRI);
+        thread2.start();
+
+        Thread thread3 = new Thread(mRI);
+        thread3.start();
+
+
+
     }
 }
---
+++ tfip/src/main/java/day06/RunnableImplement.java
@@ -0,0 +1,24 @@
+package day06;
+
+public class RunnableImplement implements Runnable {
+    
+    private String taskName;
+    
+    public RunnableImplement() {}; // Constructor
+    
+    public RunnableImplement(String taskName) {
+        this.taskName = taskName;
+    }
+
+    @Override
+    public void run() {
+        for (int i=0; i<5; i++) {
+
+            System.out.println(Thread.currentThread().getName() + "\tRunnable..."+i);
+
+        } // end of for loop
+
+    } // end of run
+
+
+}
